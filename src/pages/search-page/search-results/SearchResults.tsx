9ae660c231d76f056b84d2a77c6575b18effc89f
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-09-04 12:47:10 +0200

    remove pagination when search results are less or equal to 10

--- src/pages/search-page/search-results/SearchResults.tsx
+++ src/pages/search-page/search-results/SearchResults.tsx
@@ -1,25 +1,36 @@
 import { ReactElement, useState } from "react";
 import { useTypedLoaderData } from "../../../hooks";
-import { calculateResultPages, ICocktailResult } from "../../../utilities";
+import { calculateResultPages, ICocktail, ICocktailResult } from "../../../utilities";
 import { SearchResultItem } from "./search-result-item";
 import { Pagination } from "./pagination";
 
 export function SearchResults(): ReactElement {
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const cocktailResult = useTypedLoaderData<ICocktailResult>();
+  const cocktailResult: ICocktailResult = useTypedLoaderData<ICocktailResult>();
 
-  const resultsPerPage = 10;
-  const resultPages = calculateResultPages(cocktailResult.count, resultsPerPage);
-  const startIndex = (currentPage - 1) * resultsPerPage;
-  const endIndex = startIndex + resultsPerPage;
+  const resultsPerPage: number = 10;
+  const resultPages: number = calculateResultPages(cocktailResult.count, resultsPerPage);
+  const startIndex: number = (currentPage - 1) * resultsPerPage;
+  const endIndex: number = startIndex + resultsPerPage;
+  const resultsLessOrEqualResultsPerPage: boolean = cocktailResult.count <= resultsPerPage;
 
   // Computed cocktails depending on the currentPage
-  const cocktails = cocktailResult.drinks.slice(startIndex, endIndex);
+  const cocktails: ICocktail[] = cocktailResult.drinks.slice(startIndex, endIndex);
 
   if (cocktailResult.count === 0) {
     return (
       <div id="search-results">
         <p className="empty-list">No search results...</p>
+      </div>
+    );
+  }
+
+  if (resultsLessOrEqualResultsPerPage === true) {
+    return (
+      <div id="search-results">
+        {cocktails.map((c) => (
+          <SearchResultItem key={c.id} cocktail={c} />
+        ))}
       </div>
     );
   }
