5851f8b89750e5091924f1c7978d1c193275ece7
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-09-04 12:55:30 +0200

    add explicit types to every const variable in the requests.ts

--- src/utilities/requests.ts
+++ src/utilities/requests.ts
@@ -1,34 +1,34 @@
 import { BASE_API, mapRawCocktailToCocktail } from ".";
-import { ICocktail, ICocktailData, ICocktailResult } from "./interfaces";
+import { ICocktail, ICocktailData, ICocktailRaw, ICocktailResult } from "./interfaces";
 
 export async function getRandomCocktail(): Promise<ICocktail> {
-  const response = await fetch(`${BASE_API}/random.php`);
+  const response: Response = await fetch(`${BASE_API}/random.php`);
   const data: ICocktailData = await response.json();
-  const randomCocktailRaw = data.drinks.at(0);
-  const cocktail = mapRawCocktailToCocktail(randomCocktailRaw!);
+  const randomCocktailRaw: ICocktailRaw | undefined = data.drinks.at(0);
+  const cocktail: ICocktail = mapRawCocktailToCocktail(randomCocktailRaw!);
   return cocktail;
 }
 
 export async function getCocktailById(id: string): Promise<ICocktail> {
-  const response = await fetch(`${BASE_API}/lookup.php?i=${id}`);
+  const response: Response = await fetch(`${BASE_API}/lookup.php?i=${id}`);
   const data: ICocktailData = await response.json();
-  const cocktailByIdRaw = data.drinks.at(0);
-  const cocktailById = mapRawCocktailToCocktail(cocktailByIdRaw!);
+  const cocktailByIdRaw: ICocktailRaw | undefined = data.drinks.at(0);
+  const cocktailById: ICocktail = mapRawCocktailToCocktail(cocktailByIdRaw!);
   return cocktailById;
 }
 
 export async function getCocktailsByName(request: Request): Promise<ICocktailResult> {
-  const url = new URL(request.url);
+  const url: URL = new URL(request.url);
 
   if (url.searchParams.size === 0) return { count: 0, drinks: [] };
 
-  const name = url.searchParams.get("s"); // Value for the name query parameter
-  const response = await fetch(`${BASE_API}/search.php?s=${name}`);
+  const name: string | null = url.searchParams.get("s"); // Value for the name query parameter
+  const response: Response = await fetch(`${BASE_API}/search.php?s=${name}`);
   const data: ICocktailData = await response.json();
 
   if (data.drinks === null) return { count: 0, drinks: [] };
 
-  const cocktails = data.drinks.map((c) => mapRawCocktailToCocktail(c));
+  const cocktails: ICocktail[] = data.drinks.map((c) => mapRawCocktailToCocktail(c));
 
   return { count: data.drinks.length, drinks: cocktails };
 }
